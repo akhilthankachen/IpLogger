30c45cee71b896f68770e953cf2357749d144662
akhil thankachen <akhilthankachen1234@gmail.com>
2017-07-24 22:42:44 +0530

    values stored when visited

--- logger.php
+++ logger.php
@@ -21,8 +21,9 @@
    $sql = 'CREATE TABLE log_table( '.
          'id INT NOT NULL AUTO_INCREMENT, '.
          'ip VARCHAR(45) NOT NULL, '.
+         'hostname   TEXT NOT NULL, '.
          'current_url  TEXT NOT NULL, '.
-         'reffered_url   TEXT NOT NULL, '.
+         'reffered_url   TEXT , '.
          'date_time    timestamp(6) NOT NULL, '.
          'primary key ( id ))';
 
@@ -31,6 +32,20 @@
     if(! $retval ) {
       echo 'Could not create table: ' . mysql_error() . "\n";
     }
+    $ip_addr = $_SERVER['REMOTE_ADDR'];
+    $request_url = $_SERVER['REQUEST_URI'];
+    $refered_url = $_SERVER['HTTP_REFERER'];
+    $hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+
+    $sql = "INSERT INTO log_table ".
+       "(ip, hostname, current_url, reffered_url) ".
+       "VALUES ( '$ip_addr', '$hostname','$request_url','$refered_url')";
+
+       $retval = mysql_query( $sql, $conn );
+
+       if(! $retval ) {
+          echo 'Could not enter data: ' . mysql_error();
+       }
 
     mysql_close($conn);
 ?>
