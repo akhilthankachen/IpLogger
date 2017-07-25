1d4198e1338971a24c56f3963ae42a62971f6ece
akhil thankachen <akhilthankachen1234@gmail.com>
2017-07-25 09:51:10 +0530

    added config.ini

--- logger.php
+++ logger.php
@@ -1,20 +1,20 @@
 <?php
-   $dbhost = 'localhost:3036';
-   $dbuser = 'root';
-   $dbpass = 'root';
-   $dbname = 'IpLogger';
-   $verbose = true;
+
+   $ini = parse_ini_file('config.ini');
+
+   $dbhost = $ini['db_host'];
+   $dbuser = $ini['db_user'];
+   $dbpass = $ini['db_password'];
+   $dbname = $ini['db_name'];
+   $verbose = $ini['verbose'];
 
    function createDb($dbname,$conn){
-
      $sql = 'CREATE Database '.$dbname;
      $retval = mysql_query( $sql, $conn );
      return $retval;
-
    }
 
    function createTable($conn){
-
      $sql = 'CREATE TABLE log_table( '.
            'id INT NOT NULL AUTO_INCREMENT, '.
            'ip VARCHAR(45) NOT NULL, '.
@@ -25,23 +25,19 @@
            'primary key ( id ))';
 
      $retval = mysql_query( $sql, $conn );
-
      return $retval;
    }
 
    function insertLog($conn){
-
      $ip_addr = $_SERVER['REMOTE_ADDR'];
      $request_url = $_SERVER['REQUEST_URI'];
      $refered_url = $_SERVER['HTTP_REFERER'];
      $hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-
      $sql = "INSERT INTO log_table ".
         "(ip, hostname, current_url, reffered_url) ".
         "VALUES ( '$ip_addr', '$hostname','$request_url','$refered_url')";
 
         $retval = mysql_query( $sql, $conn );
-
         return $retval;
    }
 
@@ -52,17 +48,14 @@
 
      mysql_select_db($dbname);
      $retval = insertLog($conn);
-
      if(!$retval){
 
        if($verbose == true){
          echo "* Couldn't insert to table : ".mysql_error();
          echo "* Trying to create new database and table *";
        }
-
        createDb($dbname,$conn);
        createTable($conn);
-
        $retval_insert = insertLog($conn);
        if($retval_insert) {
          if($verbose == true){
@@ -70,24 +63,20 @@
          }
        }
        else {
-
          if($verbose == true){
            echo "* Inserted into table : failed *";
          }
-
        }
 
      }
      else {
-
        if($verbose == true){
          echo "* Inserted into table : success *";
        }
-
      }
-
    }
    else {
+
      if($verbose == true){
        echo "* Couldn't connect to database : ".mysql_error()." *";
      }
